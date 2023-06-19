0ee044e89ea5f63386138cdcc6c90a42fca59caf
Edu <efzinetti@gmail.com>
2023-06-19 17:46:41 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function convertir() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("texto-receta").style.fontWeight = "bold";
+    document.getElementById("texto-receta").style.color = "white";
+    document.getElementById("navrecetas").style.color = "white";
+    document.getElementById("hdr").style.color = "white";
+    document.getElementById("txtconv").style.color = "white";
+}
+
+
+function webmaster() {
+    alert("Eduardo Zinetti / webmaster@lasmejoresrecetas.com.ar");
+}
